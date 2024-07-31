05d11b723aa00869dd6cabb4141f9e1a9366ab5d
NathanDjunaedi <djunaedi@bu.edu>
2024-07-31 15:02:30 -0400

    more work on JSONEditor.jsx

--- src/components/Nathan/JSONEditor.jsx
+++ src/components/Nathan/JSONEditor.jsx
@@ -1,9 +1,38 @@
-import ReactJson from 'react-json-view'
+import ReactJson from 'react-json-view';
+import Select from 'react-select';
+import styled from "styled-components";
+
+const EditorWrapper= styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+const options = [
+    {value: 'base00', label: ""},
+    {value: 'base01', label: ""},
+    {value: 'base02', label: ""},
+    {value: 'base03', label: ""},
+    {value: 'base04', label: ""},
+    {value: 'base05', label: ""},
+    {value: 'base06', label: ""},
+    {value: 'base07', label: ""},
+    {value: 'base08', label: ""},
+    {value: 'base09', label: ""},
+    {value: 'base0A', label: ""},
+    {value: 'base0B', label: ""},
+    {value: 'base0C', label: ""},
+    {value: 'base0D', label: ""},
+    {value: 'base0E', label: ""},
+    {value: 'base0F', label: ""},
+]
 
 export default function JSONEditor( json ) {
 
     return(
+        <EditorWrapper>
+            <ReactJson src={json}/>
+            <Select options={options}/>
+        </EditorWrapper>
 
-        <ReactJson src={json}/>
     )
 }
