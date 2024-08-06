4832caf76f35928383a2c6add76fcce2380da665
NathanDjunaedi <natearlan@gmail.com>
2024-08-06 17:30:34 -0400

    trimmed unused imports

--- src/Context/Nathan/SelectionContext.jsx
+++ src/Context/Nathan/SelectionContext.jsx
@@ -1,39 +1,9 @@
-import {createContext, useContext, useEffect, useState} from "react";
+import {createContext, useState} from "react";
 import PropTypes from "prop-types"
-import {ColorContext} from "../Tibo/ColorContextProvider.jsx";
 
 export const SelectionContext = createContext();
     /// Context to keep track of style components for ReactJSON components
 
-let colors = {
-    '--w-rjv-color': '',
-    '--w-rjv-key-number': '',
-    '--w-rjv-key-string': '',
-    '--w-rjv-background-color': '',
-    '--w-rjv-line-color': '',
-    '--w-rjv-arrow-color': '',
-    '--w-rjv-edit-color': '',
-    '--w-rjv-info-color': '',
-    '--w-rjv-update-color': '',
-    '--w-rjv-copied-color': '',
-    '--w-rjv-copied-success-color': '',
-    '--w-rjv-curlybraces-color': '',
-    '--w-rjv-colon-color': '',
-    '--w-rjv-brackets-color': '',
-    '--w-rjv-ellipsis-color': '',
-    '--w-rjv-quotes-color': '',
-    '--w-rjv-quotes-string-color': '',
-    '--w-rjv-type-string-color': '',
-    '--w-rjv-type-int-color': '',
-    '--w-rjv-type-float-color': '',
-    '--w-rjv-type-bigint-color': '',
-    '--w-rjv-type-boolean-color': '',
-    '--w-rjv-type-date-color': '',
-    '--w-rjv-type-url-color': '',
-    '--w-rjv-type-null-color': '',
-    '--w-rjv-type-nan-color': '',
-    '--w-rjv-type-undefined-color': '',
-}
 const defaultTheme = {
     "--w-rjv-color": "",
     "--w-rjv-background-color": "",
